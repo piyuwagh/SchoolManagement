ecf7f9045153cd80beceda369daab62bce9eb941
priyanka <Priyanka@gmail.com>
2021-09-10 23:16:37 +0530

    Add project files.

---
+++ Models/MetaClasses/StudentMetadata.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagement.Models
+{
+    public class StudentMetadata
+    {
+        [Display(Name = "Student ID")]
+        public int StudentID { get; set; }
+
+        [StringLength(50)]
+        [Display(Name ="Last Name")]
+        public string LastName { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [Display(Name = "Date of Enrollment")]
+        public Nullable<System.DateTime> EnrollmentDate { get; set; }
+
+    }
+
+    [MetadataType(typeof(StudentMetadata))]
+    public partial class Student
+    {
+
+    }
+}
